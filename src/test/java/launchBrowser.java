f206b8c91c2293ae62dd594bbfd274f51f15f7d8
Mahlatse03 <mahlatsed@gmail.com>
2025-11-18 21:26:11 +0200

    Add project to git

---
+++ src/test/java/launchBrowser.java
@@ -0,0 +1,19 @@
+import com.microsoft.playwright.*;
+import org.testng.annotations.Test;
+
+public class launchBrowser {
+    @Test
+    public void launchChromium(){
+        try (Playwright playwright = Playwright.create()){
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("https://www.ndosiautomation.co.za/");
+            page.fill("#login-email", "md@gmail.com");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
